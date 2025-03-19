abbdbf71e826089c3a9cd1b12001a8242d40d5e9
ericgkrehbiel <ericgkrehbiel@gmail.com>
2025-03-19 18:08:25 -0400

    nav bar
    added nav bar and boilerplate pages

---
+++ linksies_app/lib/navigation/main_navigation.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import '../screens/feed_page.dart';
+import '../screens/profile_page.dart';
+import '../screens/messages_page.dart';
+import '../screens/settings_page.dart';
+
+class MainNavigation extends StatefulWidget {
+  @override
+  _MainNavigationState createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _selectedIndex = 0;
+  
+  static final List<Widget> _pages = [
+    FeedPage(),
+    ProfilePage(),
+    MessagesPage(),
+    SettingsPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Linksies'),
+      ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.rss_feed),
+            label: 'Feed',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'Messages',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
---
+++ linksies_app/lib/screens/feed_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FeedPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Feed Page - Links from other users will appear here'),
+    );
+  }
+}
